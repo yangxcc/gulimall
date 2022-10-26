eb0fcea4c117a4a13b1bdb5a130066673b75a0d8
yangxcc <1434890515@qq.com>
2022-10-26 11:04:20 +0800

    init project

---
+++ gulimall-logistics/src/main/java/com/yangxcc/gulimall/logistics/GulimallLogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.yangxcc.gulimall.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallLogisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallLogisticsApplication.class, args);
+    }
+
+}
